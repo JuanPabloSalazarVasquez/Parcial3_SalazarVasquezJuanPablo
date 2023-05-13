5cd79ddc46c2a7b9be2196d909dfd02db2975225
Juan Salazar <juanpasinga@gmail.com>
2023-05-13 11:01:14 -0500

    Initial configuration

---
+++ Parcial3_SalazarVasquezJuanPablo/Parcial3_SalazarVasquezJuanPablo/DAL/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace Parcial3_SalazarVasquezJuanPablo.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Nombre_de_la_tabla> Countries { get; set; }
+    }
+}
---
+++ Parcial3_SalazarVasquezJuanPablo/Parcial3_SalazarVasquezJuanPablo/Migrations/20230513053841_InitialDataBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial3_SalazarVasquezJuanPablo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
